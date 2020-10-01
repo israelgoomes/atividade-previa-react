4d81b709802727369777f66c79d7d6326dac8990
israelgoomes <rael_goomes@hotmail.com>
2020-10-01 17:40:16 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      alcool: 0,
+      gasolina: 0
+    };
+  }
+
+  calc() {
+    if (!this.state.alcool || !this.state.gasolina) {
+      alert('Digite um valor para calcular');
+      return;
+    }
+
+    let division = this.state.alcool / this.state.gasolina;
+
+    if (division < 0.7) {
+      alert('Abasteça com Álcool!')
+    } else {
+      alert('Abasteça com gasolina!')
+    }
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <h3>Descubra o combústivel mais vantajoso! </h3>
+        <div>
+          <div>
+            <label>
+              Valor do Álcool
+              <input
+                type="number"
+                min="1"
+                max="200"
+                value={this.state.alcool}
+                onChange={event =>
+                  this.setState({ alcool: event.target.value })
+                }
+              />
+            </label>
+
+            <label>
+              Valor gasolina
+              <input
+                type="number"
+                min="1"
+                max="200"
+                value={this.state.gasolina}
+                onChange={event =>
+                  this.setState({ gasolina: event.target.value })
+                }
+              />
+            </label>
+          </div>
+
+          <div>
+            <p>
+              <button class="waves-effect waves-light btn" onClick={() => this.calc()}>Calcular</button>
+            </p>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
-
-export default App;
